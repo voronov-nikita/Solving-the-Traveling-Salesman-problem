acecbbb8edb46ad66520b71aef58c32e5a3c52b7
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-05-05 22:06:16 +0300

    add-timer

--- scr/main.py
+++ scr/main.py
@@ -1,12 +1,13 @@
 from random import uniform, seed
 import matplotlib.pyplot as plt
-import itertools as it
+from itertools import permutations
+import time
 
 
 # count the point (the best is 11)
 COUNT_POINT = 10
 # random number accuracy (number of decimal places)
-ICONIC_ACCURACY = 2
+ICONIC_ACCURACY = 10
 # position points (cords)
 MIN_POSITION = -1
 MAX_POSITION = 1
@@ -25,11 +26,9 @@ def find_dist(cord1:tuple, cord2:tuple):
 
 def create_dict_varios(ls:list, start_point:int):
     dict_varios = dict()
-    # for j in range(COUNT_POINT):
     fixed_point = ls[start_point]
-    # ind_fixed_point = ls.index(fixed_point)
     copy_ls = (ls[:start_point] + ls[start_point+1:])
-    for g in it.permutations(copy_ls):
+    for g in permutations(copy_ls):
         if RETURN_START_POINT:
             g = (fixed_point, ) + g + (fixed_point, )
         else:
@@ -57,17 +56,20 @@ list_point = [
 
 
 # <------------------ Build the Way ------------------>
+start_timer = time.time()
 new_list=[[(0,0)]*10]
 
 result_function = finally_variant(list_point, START_POINT)
 if result_function is not None:
     new_list = result_function
 
-
 len_way = new_list[1]
 x = [cord[0] for cord in new_list[0]]
 y = [cord[1] for cord in new_list[0]]
 
+end_timer = time.time()
+
+print(f"TIME: {round(end_timer-start_timer, 2)} sec")
 print(f"THE START POINT: {list_point[START_POINT]}")
 print(f"LEN ALL WAY: {len_way}")
 
