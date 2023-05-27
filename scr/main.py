260758b667a0e7244330584f9ff6b8def7a654ca
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-05-27 12:14:19 +0300

    example-python-turtle

--- scr/main.py
+++ scr/main.py
@@ -7,52 +7,57 @@ import time
 # count the point (the best is 11)
 COUNT_POINT:int = 10
 # random number accuracy (number of decimal places)
-ICONIC_ACCURACY:int = 10
+ICONIC_ACCURACY:int = 12
 # position points (cords)
-MIN_POSITION:float = -1
-MAX_POSITION:float = 1
+MIN_POSITION:float = -10
+MAX_POSITION:float = 10
 
 # <--------- Additional parameters --------->
 RETURN_START_POINT:bool = True
-START_POINT:int = 0
+START_POINT:int = 3
 
 # created SEED to compare the efficiency of algorithms
 # seed(42)
 
+class FindMinWay():
 
-def find_dist(cord1:tuple, cord2:tuple) -> float:
-    return ((cord1[0]-cord2[0])**2 + (cord1[1]-cord2[1])**2)**0.5
+    def create_list_point(self, size_list:int, round_value:int, min_position:int, max_position:int):
+        # create list with tuple of cords (random values)
+        list_point = [
+            (round(uniform(min_position, max_position), round_value), round(uniform(min_position, max_position), round_value)) for _ in range(size_list)
+        ]
+        return list_point
 
-
-def create_dict_varios(ls:list, start_point:int) -> dict:
-    dict_varios = dict()
-    fixed_point = ls[start_point]
-    copy_ls = (ls[:start_point] + ls[start_point+1:])
-    for g in permutations(copy_ls):
-        if RETURN_START_POINT:
-            g = (fixed_point, ) + g + (fixed_point, )
-        else:
-            g = (fixed_point, ) + g
-        s=sum([find_dist(g[i], g[i+1]) for i in range(len(g)-1)])
-
-        dict_varios[g] = s
-    
-    return dict_varios
+    def find_dist(self, cord1:tuple, cord2:tuple) -> float:
+        return ((cord1[0]-cord2[0])**2 + (cord1[1]-cord2[1])**2)**0.5
 
 
-def finally_variant(ls:list, start:int):
-    new_varios = create_dict_varios(ls, start)
-    min_value = min(new_varios.values())
+    def create_dict_varios(self, ls:list, start_point:int) -> dict:
+        dict_varios = dict()
+        fixed_point = ls[start_point]
+        copy_ls = (ls[:start_point] + ls[start_point+1:])
+        for g in permutations(copy_ls):
+            if RETURN_START_POINT:
+                g = (fixed_point, ) + g + (fixed_point, )
+            else:
+                g = (fixed_point, ) + g
+            s=sum([self.find_dist(g[i], g[i+1]) for i in range(len(g)-1)])
 
-    for key, value in new_varios.items():
-        if value == min_value:
-            return (list(key), value)
+            dict_varios[g] = s
         
+        return dict_varios
 
-# create list with tuple of cords (random values)
-list_point = [
-    (round(uniform(MIN_POSITION, MAX_POSITION), ICONIC_ACCURACY), round(uniform(MIN_POSITION, MAX_POSITION), ICONIC_ACCURACY)) for _ in range(COUNT_POINT)
-]
+
+    def finally_variant(self, ls:list, start:int):
+        new_varios = self.create_dict_varios(ls, start)
+        min_value = min(new_varios.values())
+
+        for key, value in new_varios.items():
+            if value == min_value:
+                return (list(key), value)
+            
+
+
 # create grafwith tuple of cords
 graf_point = [
     (-1, -1), (-1, 0), (-1, 1),
@@ -62,25 +67,30 @@ graf_point = [
 
 
 # <------------------ Build the Way ------------------>
-start_timer = time.time()
-new_list=[[(0,0)]*10]
+if __name__=="__main__":
+    start_timer = time.time()
+    new_list=[[(0,0)]*10]
 
-result_function = finally_variant(list_point, START_POINT)
-if result_function is not None:
-    new_list = result_function
+    minway = FindMinWay()
+    list_point = minway.create_list_point(COUNT_POINT, ICONIC_ACCURACY, MIN_POSITION, MAX_POSITION)
+    result_function = minway.finally_variant(list_point, START_POINT)
+    if result_function is not None:
+        new_list = result_function
 
-len_way = new_list[1]
-x = [cord[0] for cord in new_list[0]]
-y = [cord[1] for cord in new_list[0]]
+    len_way = new_list[1]
+    x = [cord[0] for cord in new_list[0]]
+    y = [cord[1] for cord in new_list[0]]
 
-end_timer = time.time()
+    end_timer = time.time()
 
-print(f"TIME: {round(end_timer-start_timer, 2)} sec")
-print(f"THE START POINT: {list_point[START_POINT]}")
-print(f"LEN ALL WAY: {len_way}")
+    print("="*130)
+    print(f"TIME: {round(end_timer-start_timer, 2)} sec")
+    print(f"THE START POINT: {list_point[START_POINT]}")
+    print(f"LEN ALL WAY: {len_way}")
+    print("="*130)
 
-plt.plot(x, y)
-# plt.grid(True)
-plt.scatter(x, y)
-plt.scatter(x[-1], y[-1], color="red")
-plt.show()
+    plt.plot(x, y)
+    # plt.grid(True)
+    plt.scatter(x, y)
+    plt.scatter(x[-1], y[-1], color="red")
+    plt.show()
---
+++ scr/turtle-example.py
@@ -0,0 +1,48 @@
+import turtle 
+from time import sleep
+
+from main import FindMinWay
+
+find_min_way = FindMinWay()
+
+point_draw = turtle.Turtle(visible=False)
+point_draw.pencolor("blue")
+point_draw.pensize(4)
+point_draw.speed(0)
+
+k = turtle.Turtle()
+k.pensize(2)
+k.pencolor("red")
+k.speed(1)
+
+
+def draw_point(list_point:list, start_point:int):
+    # point_draw.penup()
+    for elem in range(len(list_point)):
+        point_draw.pendown()
+        if elem == start_point:
+            point_draw.pencolor("red")
+            k.penup()
+            k.goto(list_point[elem][0], list_point[elem][1])
+            k.pendown()
+        else:
+            point_draw.pencolor("blue")
+
+        point_draw.penup()
+        point_draw.goto(list_point[elem][0], list_point[elem][1])
+        point_draw.pendown()
+        point_draw.dot()
+        
+# <<<<<<<<<<<<<<<< START >>>>>>>>>>>>>>>>>
+list_points = find_min_way.create_list_point(8, 0, -200, 200)
+
+draw_point(list_points, 0)
+try:
+    for g in find_min_way.finally_variant(list_points, 0)[0]:
+        k.goto(g[0], g[1])
+except:
+    pass
+
+
+
+turtle.done()
